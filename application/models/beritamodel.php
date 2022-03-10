d3718f2ac20c2e7de312d6aeb820dcedd90191bf
futri_nurlaili <hissaniahdila@gmail.com>
2022-03-10 15:18:38 +0700

    ini blog tentang ipa dan ips

--- application/models/beritamodel.php
+++ application/models/beritamodel.php
@@ -5,11 +5,11 @@
             return $this->db->get('tbl_berita')->result_array();
         }
             public function add_berita($tambah){
-                return $this->db->insert('tbl_berita',$tambah);
+                return $this->db->insert('tbl_berita',$tambah); 
         }
         public function insertberita($data){
             $this->db->insert('berita',$data);
-            return $this->db->insert_id();
+            return $this->db->insert_id(); 
         }                                                                                                                                             
     }
  ?>
